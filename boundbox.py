1f2d4c7c224f637ac4f39f026309d7ee71d719e1
juniorsoares35 <126364188+juniorsoares35@users.noreply.github.com>
2024-07-31 22:23:12 -0300

    Descricao boundbox.py

--- boundbox.py
+++ boundbox.py
@@ -4,13 +4,16 @@ c = creopyson.Client()
 c.connect()
 id = c.sessionId
 creo_open = c.is_creo_running()
+
+# Detecta se o creo esta aberto , caso contrario fecha o servidor
 if creo_open == False:
     input(print("creo nao foi aberto corretamente"))
     exit()
 
-
+# Lê todos os arquivos .prt do projeto
 parts = c.creo_list_files("*prt")
 
+# remove din_912 da busca
 for part in parts:
     if "912" in part:
         part.remove(part)
@@ -21,7 +24,6 @@ for part in parts:
         print(part)
 
     coord = creopyson.geometry.bound_box(c, file_=part)
-    # print(coord)
 
     x = coord["xmax"], abs(coord["xmin"])
     x = x[0] + x[1]
@@ -37,4 +39,5 @@ for part in parts:
 
     relation = 'X = "{}"\nY="{}"\nZ="{}"'.format(x, y, z)
 
+    # Altera as relations da peca com a variavel relation
     c.file_relations_set(file_=part, relations=relation)
